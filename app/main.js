a900a4e9c28fd1c8802055657f7fcd49b0dbbdcc
Knight-Kaizen <auferstehung20@gmail.com>
2024-09-13 23:46:54 +0530

    add dns-header response in reply

--- app/main.js
+++ app/main.js
@@ -6,11 +6,11 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 //
 const udpSocket = dgram.createSocket("udp4");
-udpSocket.bind(2053, "127.0.0.1");
+
 
 udpSocket.on("message", (buf, rinfo) => {
   try {
-    const response = Buffer.from("");
+    const response = Buffer.from("1234100000000000");
     udpSocket.send(response, rinfo.port, rinfo.address);
   } catch (e) {
     console.log(`Error receiving data: ${e}`);
@@ -25,3 +25,5 @@ udpSocket.on("listening", () => {
   const address = udpSocket.address();
   console.log(`Server listening ${address.address}:${address.port}`);
 });
+
+udpSocket.bind(2053, "127.0.0.1");
